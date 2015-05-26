b59bb9902740e6245417be72a830897ca3989a51
Kenn Costales <fxkennyfrc@gmail.com>
2015-05-27 03:11:46 +0800

    topics-and-posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
 
-
    devise_for :users
 
    resources :questions
 
    resources :advertisements
    
-   resources :posts
+   resources :topics do
+     resources :posts, except: [:index]
+   end
 
   get 'about' => 'welcome#about'
 
