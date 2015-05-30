f0ad0401e43cd48c0a66e611d39a6ddb937ddbd3
Kenn Costales <fxkennyfrc@gmail.com>
2015-05-30 17:31:49 +0800

    validating-posts done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ActiveRecord::Base
-    has_many :comments
-    belongs_to :user
-    belongs_to :topic
+   has_many :comments
+   belongs_to :user
+   belongs_to :topic
 
-    default_scope { order('title ASC') }
-    scope :reverse_created_at, -> { order('created_at DESC') }
-end
+   default_scope { order('created_at DESC') }
+
+   validates :title, length: { minimum: 5 }, presence: true
+   validates :body, length: { minimum: 20 }, presence: true
+   validates :topic, presence: true
+   validates :user, presence: true
+ end
